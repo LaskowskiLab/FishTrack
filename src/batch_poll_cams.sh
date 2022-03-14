3caeb616cff6a0f7f3d26a964c511602095a408f
aperkes <perkes.ammon@gmail.com>
2022-03-14 16:03:27 -0700

    improved book keeping

--- src/batch_poll_cams.sh
+++ src/batch_poll_cams.sh
@@ -1,8 +1,12 @@
+
+rm ~/Documents/Scripts/FishTrack/recording/now/*.jpg
+
 for i in $(cat $1); do
 ssh $i << EOF
     raspistill -o ~/Desktop/CCtest/test.jpg
     exit
 EOF
-scp $i:~/Desktop/CCtest/test.jpg ./test_imgs/$i.jpg
+
+scp $i:~/Desktop/CCtest/test.jpg ~/Documents/Scripts/FishTrack/recording/now/$i.jpg
 
 done
