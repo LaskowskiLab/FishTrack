362a9a0ded05905d28364f16a8417cf6191caeae
aperkes <perkes.ammon@gmail.com>
2022-03-14 13:31:11 -0700

    updates for video

--- recording/src/run_vid.sh
+++ recording/src/run_vid.sh
@@ -31,7 +31,7 @@ mkdir -p $directory_path
 
 ## Add the -n flag or --nopreview to turn off preview, this might help with dropped frames
 #raspistill -t 43200000 -tl 1000 --nopreview -vf -hf -q 20 -h 500 -w 500 -o $directory_path$pi_name.$year_stamp.%01d.jpg -dt
-raspivid --width 500 --height 500 --framerate 1 --qp 17 --timeout $((14*60*60*1000)) ---output $directory_path$pi_name.$date_stamp 
+raspivid --width 1080 --height 1080 --framerate 1 --qp 17 --timeout $((12*60*60*1000)) --output $directory_path$pi_name.$date_stamp.h264
 
 ## Use this line if the video will be too big:
-#raspivid --width 500 --height 500 --framerate 1 --qp 17 --timeout $((14*60*60*1000)) --segment $((60*60*1000)) ---output $directory_path$pi_name.$date_stamp-%02d.mp4
+#raspivid --width 500 --height 500 --framerate 1 --qp 17 --timeout $((14*60*60*1000)) --segment $((60*60*1000)) --output $directory_path$pi_name.$date_stamp-%02d.h264
