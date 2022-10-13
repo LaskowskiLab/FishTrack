a3a4272fccede1063f555786ff3d7e4f8bf4f6b6
aperkes <perkes.ammon@gmail.com>
2022-10-13 16:27:38 -0700

    updates to allow more flexible run_vid, experiments for kirsten

--- recording/src/run_vid.sh
+++ recording/src/run_vid.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+n_hours = ${1-12}
+f_rate = ${2-1}
+
 ## Find the name, regardless of the pi.
 pi_name=${HOSTNAME: -4}
 if [[ $pi_name == "rypi" ]]; then
@@ -15,6 +18,7 @@ year_stamp=$(date "+%Y.%m")
 ## using unix timestamp (-ts) would probably save milliseconds? 
 
 date_stamp=$(date "+%Y.%m.%d")
+dt_stamp=$(date "+%Y.%m.%d.%H.%M")
 
 ## Be sure to check the parent directory exists
 
@@ -31,7 +35,7 @@ mkdir -p $directory_path
 
 ## Add the -n flag or --nopreview to turn off preview, this might help with dropped frames
 #raspistill -t 43200000 -tl 1000 --nopreview -vf -hf -q 20 -h 500 -w 500 -o $directory_path$pi_name.$year_stamp.%01d.jpg -dt
-raspivid --width 1080 --height 1080 --framerate 1 --qp 17 --nopreview --timeout $((12*60*60*1000)) --output $directory_path$pi_name.$date_stamp.h264
+raspivid --width 1080 --height 1080 --framerate $f_rate --qp 17 --nopreview --timeout $(($n_hours*60*60*1000)) --output $directory_path$pi_name.$dt_stamp.h264
 
 ## Use this line if the video will be too big:
 #raspivid --width 500 --height 500 --framerate 1 --qp 17 --timeout $((14*60*60*1000)) --segment $((60*60*1000)) --output $directory_path$pi_name.$date_stamp-%02d.h264
