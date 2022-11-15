f215c3fd35316fa48ed498edb36b24cc1cd861cd
aperkes <perkes.ammon@gmail.com>
2022-11-15 08:02:04 -0800

    update for kirsten

--- src/update_pi_src.sh
+++ src/update_pi_src.sh
@@ -1,7 +1,8 @@
 for i in $(cat $1); do
 ssh $i << EOF
+    pkill raspivid
     rclone copy AmazonBox:/src/ ~/recording/src/
-    crontab ~/recording/src/crontab-pi.txt
+    crontab ~/recording/src/crontab-kirsten.txt
     exit
 EOF
 
