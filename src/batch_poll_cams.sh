707c70bf557c28affe9637be304f632869c81b12
aperkes <perkes.ammon@gmail.com>
2023-04-19 11:30:46 -0700

    updated to skip host checking

--- src/batch_poll_cams.sh
+++ src/batch_poll_cams.sh
@@ -2,11 +2,11 @@
 rm ~/Documents/Scripts/FishTrack/recording/now/*.jpg
 
 for i in $(cat $1); do
-ssh $i << EOF
-    raspistill -o ~/Desktop/CCtest/test.jpg
+ssh -o StrictHostKeyChecking=no $i << EOF
+    raspistill -o ~/recording/cam_check.jpg
     exit
 EOF
 
-scp $i:~/Desktop/CCtest/test.jpg ~/Documents/Scripts/FishTrack/recording/now/$i.jpg
+scp $i:~/recording/cam_check.jpg ~/Documents/Scripts/FishTrack/recording/now/$i.jpg
 
 done
