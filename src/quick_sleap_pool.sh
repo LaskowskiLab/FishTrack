1864c4ae0964256d067067e86db0b1b53a229053
aperkes <perkes.ammon@gmail.com>
2023-06-09 12:47:28 -0700

    new version to deal with Jon pools

---
+++ src/quick_sleap_pool.sh
@@ -0,0 +1,21 @@
+
+center_dict='/home/ammon/Documents/Scripts/FishTrack/src/center_dict.22.02.25.tsv'
+model_dir='/home/ammon/Documents/Scripts/FishTrack/sleap/models'
+
+export PATH="/home/ammon/anaconda3/bin:$PATH"
+source activate sleap
+
+source_dir="/home/ammon/Documents/Scripts/FishTrack/sleap/KirstenSLEAP/"
+
+
+for f in $source_dir*.crop.mp4
+do
+    video_file=${f#"$source_dir"}
+    pi_id="${video_file::4}"
+    echo working on $video_file
+
+## You will need to change your models here
+    sleap-track -m $model_dir/finetuned352.centroid -m $model_dir/finetuned352.centered_instance --peak_threshold .4 --tracking.tracker flow --tracking.similarity iou $f
+    sleap-convert $f.predictions.slp --format analysis -o $f.h5
+    python process_h5.py -i $f.h5 -c $pi_id
+done
