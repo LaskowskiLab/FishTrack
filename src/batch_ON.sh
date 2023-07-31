17d62e76d82bd1cf77fca7017464a6bad9ec41cf
aperkes <perkes.ammon@gmail.com>
2023-07-31 13:03:58 -0700

    fixed some little bugs, now it works

--- src/batch_ON.sh
+++ src/batch_ON.sh
@@ -1,13 +1,16 @@
 for i in $(cat $1); do
 
 echo $i
+
 ssh -o StrictHostKeyChecking=no $i << EOF
     rclone copy AmazonBox:/src/ ~/recording/src/
-    copy ~/recording/src/aliases.txt ~/.bash_aliases
-    . ~/.bash_aliases
-    schedule_ON ${2:-rogue}
+    cp ~/recording/src/aliases.txt ~/.bash_aliases
     exit
 EOF
+
+## This has to be a separate line so that it can use command arguments here
+ssh -o "StrictHostKeyChecking=no" $i "bash /home/pi/recording/src/schedule_on.sh ${2:-rogue}"
+
 echo 'End ' $i
 
 done
