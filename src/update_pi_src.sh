9d36ee1f1b2e71a21c384167882e5924af91c92a
aperkes <perkes.ammon@gmail.com>
2023-07-31 13:04:39 -0700

    added alias updating to the update script

--- src/update_pi_src.sh
+++ src/update_pi_src.sh
@@ -1,6 +1,7 @@
 for i in $(cat $1); do
-ssh $i << EOF
+ssh -o "StrictHostKeyChecking=no" $i << EOF
     rclone copy AmazonBox:/src/ ~/recording/src/
+    cp ~/recording/src/aliases.txt ~/.bash_aliases
     exit
 EOF
 
