ff28798f69ff624c0582fac59edcae4653bd4d41
aperkes <perkes.ammon@gmail.com>
2023-07-31 13:03:58 -0700

    fixed some little bugs, now it works

--- src/batch_OFF.sh
+++ src/batch_OFF.sh
@@ -1,13 +1,14 @@
 for i in $(cat $1); do
 
 echo $i
+
 ssh -o StrictHostKeyChecking=no $i << EOF
     rclone copy AmazonBox:/src/ ~/recording/src/
-    copy ~/recording/src/aliases.txt ~/.bash_aliases
-    . ~/.bash_aliases
-    schedule_OFF
+    cp ~/recording/src/aliases.txt ~/.bash_aliases
+    bash ~/recording/src/schedule_off.sh
     exit
 EOF
+
 echo 'End ' $i
 
 done
