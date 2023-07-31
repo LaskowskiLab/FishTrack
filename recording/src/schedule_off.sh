ca030463040ecbaa3b72e816a472a4eb606163d7
aperkes <perkes.ammon@gmail.com>
2023-07-31 12:37:09 -0700

    pause is more descriptive than off

--- recording/src/schedule_off.sh
+++ recording/src/schedule_off.sh
@@ -1,7 +1,7 @@
 ## Script to schedule pi's on
 
 ## Set suffix as first input, or "rogue" if there is none. 
-suffix=${1:-OFF}
+suffix=${1:-PAUSED}
 
 ## This shouldn't matter, but is a backup against people forgetting
 bash ~/recording/src/set_suffix.sh $suffix
