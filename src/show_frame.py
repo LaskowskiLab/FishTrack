1df6ba14d91784914a6286f835ca0a6179aa95a6
aperkes <perkes.ammon@gmail.com>
2023-08-02 11:02:12 -0700

    little script to show a frame so you can get the crop values

---
+++ src/show_frame.py
@@ -0,0 +1,31 @@
+import cv2
+import sys
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+in_file = sys.argv[1]
+cap = cv2.VideoCapture(in_file)
+
+while True:
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('Frame',frame)
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            if 'y' in input('Happy?'):
+                break
+            else:
+                pass
+
+cap.release()
+cv2.destroyAllWindows()
+
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+fig,ax = plt.subplots()
+gray = np.flipud(gray)
+
+ax.imshow(gray)
+
+plt.show()
