102e802e42778c57b2c03be168ab1d1b5eb558cb
aperkes <perkes.ammon@gmail.com>
2023-08-02 10:02:26 -0700

    exclude suffix so it keeps the user setting

--- src/update_pi_src.sh
+++ src/update_pi_src.sh
@@ -1,6 +1,6 @@
 for i in $(cat $1); do
 ssh -o "StrictHostKeyChecking=no" $i << EOF
-    rclone copy AmazonBox:/src/ ~/recording/src/
+    rclone copy --exclude suffix.txt AmazonBox:/src/ ~/recording/src/
     cp ~/recording/src/aliases.txt ~/.bash_aliases
     exit
 EOF
