2bdb4653d2669116f2deac884ee1fd98e61cd10d
aperkes <perkes.ammon@gmail.com>
2023-08-14 14:48:03 -0700

    lots of debugging, also changed output stats

--- src/process_h5.py
+++ src/process_h5.py
@@ -10,7 +10,8 @@ import argparse
 
 from matplotlib import pyplot as plt
 
-CENTER = [349,425]
+#CENTER = [349,425]
+CENTER = [400,450]
 
 ## Code which takes .h5 files (output from SLEAP) and processes it to usable data
 
@@ -40,6 +41,7 @@ def build_parse():
     parser.add_argument('--visualize','-v',action='store_true',help='Visualize plot, defaults to False')
     parser.add_argument('--dump','-d',action='store_true',help='Debug option to prevent saving output')
     parser.add_argument('--spots','-f',required=False,help='Path to mp4 of spotlighted video from spotlight.py')
+    parser.add_argument('--project_csv','-p',required=False,help='Path to a project csv where all the summary statistics will be written')
     return parser.parse_args()
 
 ## Input: Takes a track (1 fish, all nodes, all frames)
@@ -61,7 +63,7 @@ def smooth_diff(track,win=25,poly=3):
     return simple_vel
 
 ## Read through all the frames in a track and allocate them to correct quadrants
-def correct_track(locations,track_occupancy,t,center_point=CENTER):
+def correct_track(locations,track_occupancy,instance_scores,t,center_point=CENTER):
     frames = track_occupancy[t] == 1
     n_frames,n_nodes,_,n_tracks = locations.shape
 
@@ -69,40 +71,46 @@ def correct_track(locations,track_occupancy,t,center_point=CENTER):
     med_point = np.nanmedian(track,axis=0)
     primary_loc = get_quadrant(med_point)
     new_occupancy = np.zeros([4,n_frames])
+    new_scores = np.full([n_frames,4],np.nan)
+
     new_quads = np.full_like(new_occupancy,np.nan)
 
     new_locations = np.full([n_frames,n_nodes,2,4],np.nan)
-    for f_ in np.argwhere(frames):
+    #import pdb;pdb.set_trace()
+    for f_ in np.argwhere(frames)[:,0]:
         f_loc = get_quadrant(track[f_,:])
         if f_loc != primary_loc:
-            new_quads[f_loc,f_] = 1
-            new_occupancy[f_,f_loc] = 1
+            new_quads[f_loc,f_] = f_loc
+            new_occupancy[f_loc,f_] = 1
+            new_scores[f_,f_loc] = instance_scores[f_,t]
             new_locations[f_,:,:,f_loc] = locations[f_,:,:,t]
+
             locations[f_,:,:,t] = np.nan
             track_occupancy[t,f_] = 0
-    if len(quads) > 1:
-        for q in quads:
-            q_frames = frame_quads == q
-            new_locations[q_frames,:,:,q] = locations[q_frames,:,:,t]
+            instance_scores[f_,t] = np.nan
     not_0 = np.sum(new_occupancy,axis=1) != 0
     new_locations = new_locations[:,:,:,not_0]
     new_occupancy = new_occupancy[not_0,:]
+    new_scores = new_scores[:,not_0]
     new_quads = new_quads[not_0,:]
-    return new_locations,new_occupancy,primary_loc,new_quads
+    #import pdb;pdb.set_trace() 
+    return new_locations,new_occupancy,new_scores,primary_loc,new_quads
 
 
 ## This assigns tracks to a unique quadrant. If possibe
 ###  If tracks are split across 2-4 quads, it divides them and adds new tracks
-def track_to_quad(locations,track_occupancy,center_point = CENTER):
+def track_to_quad(locations,track_occupancy,instance_scores,center_point = CENTER):
     n_tracks = np.shape(locations)[3]
     n_frames = np.shape(locations)[0]
     track_quad = []
+    all_quads = []
     quad_array = np.full(np.shape(track_occupancy),np.nan)
-    all_quads = np.array([0,n_frames])
+    all_quad_arrays = np.empty([0,n_frames])
 
 ## I make these because I'm going to be editing them in a function later
     all_locations = np.array(locations)
     all_occupancy = np.array(track_occupancy)
+    all_instance_scores = np.array(instance_scores)
     for t in range(n_tracks):
         frames = track_occupancy[t] == 1 ## Needs to do the bool here.
         track = locations[frames,:,:,t]
@@ -120,22 +128,27 @@ def track_to_quad(locations,track_occupancy,center_point = CENTER):
             # this track is in two places, that's trouble. 
 
 ## Note, I am deleting the overlap within the function
-            new_locations,new_occupancy,f_loc,new_quads = correct_track(locations,track_occupancy,t,center_point)
-            all_locations = np.hstack(all_locations,new_locations]
-            all_occupancy = np.hstack(all_occupancy,new_occupancy)
-            all_quads = np.hstack(all_quads,new_quads)
+            new_locations,new_occupancy,new_instance_scores,f_loc,new_quad_array = correct_track(locations,track_occupancy,instance_scores,t,center_point)
+            #import pdb;pdb.set_trace()
+            all_locations = np.concatenate([all_locations,new_locations],axis=3)
+            all_occupancy = np.concatenate([all_occupancy,new_occupancy],axis=0)
+            all_instance_scores = np.concatenate([all_instance_scores,new_instance_scores],axis=1)
 
+            all_quad_arrays = np.concatenate([all_quad_arrays,new_quad_array],axis=0)
+            all_quads.extend(np.nanmin(new_quad_array,1).astype(int))
         #f_loc = get_quadrant(np.nanmean(track,axis=(0,1)),center_point)
+            #import pdb;pdb.set_trace() 
 
         quad_array[t,track_occupancy[t]] = f_loc
         track_quad.append(f_loc)
-    
-    return track_quad, quad_array,[all_locations,all_occupancy]
+    track_quad.extend(all_quads)
+    #import pdb;pdb.set_trace()
+    return track_quad, quad_array,[all_locations,all_occupancy,all_instance_scores]
 
 ## You know, like a quadrant bottle neck
 def quadle_neck(locations,track_occupancy,instance_scores):
-    track_quad,quad_array,cleaned_data = track_to_quad(locations,track_occupancy) 
-    locations,track_occupancy = cleaned_data
+    track_quad,quad_array,cleaned_data = track_to_quad(locations,track_occupancy,instance_scores) 
+    locations,track_occupancy,instance_scores = cleaned_data
     n_tracks = np.shape(locations)[3]
     for t in range(n_tracks): 
         frames = track_occupancy[t] == 1
@@ -153,7 +166,7 @@ def quadle_neck(locations,track_occupancy,instance_scores):
                     track_occupancy[t] = 0
                     #track_occupancy[t_,frames] = 0 ## or nan?
                     break
-    return locations,track_occupancy
+    return locations,track_occupancy,instance_scores
 
 ## Only keep frames that overlap with background subtraction.
 def spot_filter(locations,track_occupancy,spot_file):
@@ -181,7 +194,6 @@ def spot_filter(locations,track_occupancy,spot_file):
                 #print('found 0')
                 if np.sum(frame[y-10:y+10,x-10:x+10,0]) == 0: ### Row column notation
                     #print('deleting:',t,f_index,frame[y,x])
-                    #import pdb; pdb.set_trace()
                     track_occupancy[t,f_index] = 0 ## Just delete that frame
             f_index += 1
             count += 1
@@ -221,6 +233,7 @@ if __name__ == "__main__":
             width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
             cap.release()
             center_point = [width //2, height//2]
+            center_point = CENTER
         except:
             print('Nevermind, using default:',CENTER)
             center_point = CENTER
@@ -236,26 +249,35 @@ if __name__ == "__main__":
     CENTER = center_point
 
     print('using center point:',center_point)
-    n_frames = len(locations)
-    n_tracks = np.shape(locations)[3]
-    n_nodes = len(node_names)
 
 ## Create an stacked array for all the tracks
 ## This can't be this big, it breaks.
 
     print('clearing overlapping tracks')
 ## This actually reshapes locations and track_occupancy
-    locations_1,track_occupancy_1 = quadle_neck(np.array(locations),np.array(track_occupancy),instance_scores)
+    locations_1,track_occupancy_1,instance_scores_1 = quadle_neck(np.array(locations),np.array(track_occupancy),instance_scores)
+    locations,track_occupancy,instance_scores = locations_1,track_occupancy_1,instance_scores_1
 
     print('running spot filter')
-    locations_2,track_occupancy_2 = spot_filter(np.array(locations_1),np.array(track_occupancy_1),args.spots)
-    cleaned_tracks = np.full([n_fish,n_frames,n_nodes,2],np.nan)
+    if args.spots is not None:
+        locations_2,track_occupancy_2 = spot_filter(np.array(locations_1),np.array(track_occupancy_1),args.spots)
+        locations,track_occupancy = locations_2,track_occupancy_2
+    #import pdb;pdb.set_trace()
 
     error_count = 0
+
+    n_frames = len(locations)
+    n_tracks = np.shape(locations)[3]
+    n_nodes = len(node_names)
+
+    cleaned_tracks = np.full([n_fish,n_frames,n_nodes,2],np.nan)
 #for t in tqdm(range(n_tracks)):
+    print('center point:',center_point)
     for t in range(n_tracks):
         split_track = False
         frames = track_occupancy[t] == 1 ## Needs to do the bool here.
+        if np.sum(frames) == 0: ## this occurs when it's been cleaned by above process
+            continue
         track = locations[frames,:,:,t]
 
         med_track = np.nanmedian(track,axis=1)
@@ -265,12 +287,16 @@ if __name__ == "__main__":
         loc_min = get_quadrant(min_points)
         loc_max = get_quadrant(max_points)
 
+        #print(min_points,max_points)
+        #print(loc_min,loc_max)
+
         if loc_min == loc_max:
             f_loc = loc_min
             cleaned_tracks[f_loc,frames,:,:] = locations[frames,:,:,t]
         else: ## if the track enteres two different quadrants, it's probably two different fish
             error_count += 1
 ## This approach is ok, but it will just keep whichever track comes second
+            #continue
             for f in np.arange(n_frames)[frames]:
                 f_loc = get_quadrant(np.nanmean(locations[f,:,:,t],axis=0),center_point)
                 cleaned_tracks[f_loc,f,:,:] = locations[f,:,:,t]
@@ -282,38 +308,48 @@ if __name__ == "__main__":
 #averaged_tracks = np.nanmean(cleaned_tracks,axis=4)
 
 #print(averaged_tracks.shape)
-
     fig,ax = plt.subplots()
 
     print('error count:',error_count,'of',n_frames)
     visible_frames = np.zeros(n_fish)
     proportion_visible = np.zeros(n_fish)
     velocities = []
-    center_ratio = []
+    activities = []
+    corner_ratios = []
+    y_max = np.nanmax(cleaned_tracks[:,:,:,1])
+    ax.axvline(CENTER[0],color='black')
+    ax.axhline(CENTER[1],color='black')
+    cleaned_med = np.nanmedian(cleaned_tracks,axis=2)
     for f in range(n_fish):
-        ax.scatter(cleaned_tracks[f,:,4,0],400-cleaned_tracks[f,:,4,1],alpha=.05,marker='.')
-        #ax.plot(cleaned_tracks[f,:,4,0],400-cleaned_tracks[f,:,4,1],alpha=.1)
+        #ax.scatter(cleaned_tracks[f,:,4,0],y_max - cleaned_tracks[f,:,4,1],alpha=.05,marker='.')
+        #ax.scatter(cleaned_med[f,:,0],y_max - cleaned_med[f,:,1],alpha=.05,marker='.')
+        ax.scatter(cleaned_med[f,:,0],cleaned_med[f,:,1],alpha=.1,marker='.')
         n_vis = np.sum(~np.isnan(cleaned_tracks[f,:,0,0]))
         visible_frames[f] = n_vis
         proportion_visible[f] = n_vis / n_frames
+
         velocity = smooth_diff(cleaned_tracks[f,:,0])
         median_velocity = np.nanmedian(velocity)
         velocities.append(median_velocity)
 
+        prop_active = np.nansum(velocity > 5) / np.sum(~np.isnan(velocity))
+        activities.append(prop_active)
         visible_track = cleaned_tracks[f,:,4][~np.isnan(cleaned_tracks[f,:,4,0])]
-        xs = np.abs(visible_track[:,0] - center_point[0]) < 30
-        ys = np.abs(visible_track[:,1] - center_point[1]) < 30
-        courage_count = np.sum(np.logical_or(xs,ys))
-        courage_ratio = courage_count / len(visible_track)
-        center_ratio.append(courage_ratio)
+
+        xs = np.abs(visible_track[:,0] - center_point[0]) > center_point[0]/2
+        ys = np.abs(visible_track[:,1] - center_point[1]) > center_point[1]/2
+        coward_count = np.sum(np.logical_and(xs,ys))
+        coward_ratio = coward_count / len(visible_track)
+        corner_ratios.append(coward_ratio)
 
     print(':: STATS ::')
     print('proportion visible:',proportion_visible)
     print('mean velocity:',velocities)
-    print('proportion away from edge:',center_ratio)
+    #print('proportion away from edge:',center_ratio)
 
     if args.visualize:
         fig.show()
+        plt.gca().invert_yaxis()
         plt.show()
 
     if not args.dump:
@@ -325,7 +361,19 @@ if __name__ == "__main__":
             f.write(':: STATS ::')
             f.write('\nproportion visible: ' + str(np.round(proportion_visible,3)))
             f.write('\nmean velocity: ' + str(np.round(velocities,3)))
-            f.write('\nproportion away from edge: ' + str(np.round(center_ratio,3)))
+            #f.write('\nproportion away from edge: ' + str(np.round(corner_ratios,3)))
+        if args.project_csv is not None:
+            with open(project_csv,'w') as f:
+                vid_name = in_file.split('/')[-1]
+                delim = ','
+                for f in range(n_fish):
+                    quad = str(get_quadrant(np.nanmedian(cleaned_med[f],axis=0)))
+                    prop_visible = str(round(proportion_visible[f],3))
+                    vel = str(round(velocities[f],3))
+                    activity = str(round(prop_moving[f],3))
+                    boldness = str(round(1-corner_ratio[f],3))
+                    f_line = delim.join(vid_name,quad,prop_visible,vel,activity,boldness)
+                    f.write(f_line + '\n')
         columns = ['Frame','Fish','x','y']
         frame_list,fish_list,x_list,y_list = [],[],[],[]
         for f in range(n_fish):
