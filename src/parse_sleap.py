4453046071f950448401e195a1001c094c02b183
aperkes <perkes.ammon@gmail.com>
2023-08-24 16:17:55 -0700

    some new functions for cleaning tracks

--- src/parse_sleap.py
+++ src/parse_sleap.py
@@ -3,6 +3,7 @@
 ## Code to process the .npz readouts from Trex
 ## written by Ammon for use in the Laskowski Lab, for questions contact perkes.ammon@gmail.com
 
+import h5py
 import numpy as np
 import argparse
 from matplotlib import pyplot as plt
@@ -17,6 +18,14 @@ def build_parse():
     parser.add_argument('--showplot','-p',required=False,action='store_true',help='Optionally visualize the plot')
     parser.add_argument('--saveplot','-s',required=False,action='store_true',help='Optionally save the plot')
     return parser.parse_args()
+
+def read_h5(h5_file):
+    with h5py.File(h5_file,'r') as f:
+        locations = f['tracks'][:].T
+        track_occupancy = f['track_occupancy'][:].T
+        video_path = str(f['video_path'][()])[2:-1]
+        instance_scores = f['instance_scores'][:].T
+    return locations,track_occupancy,instance_scores
 
 ## Parse the file string to get relevent info. Requires string to be parsed correctly
 def get_meta(input_path):
@@ -63,6 +72,7 @@ def clear_lone_points(a):
     a[1:-1] = y
     return a
 
+## Takes Frames x XY track
 def clear_teleports(a,max_speed = 500):
     a = np.array(a)
     dx = np.abs(np.diff(a[:,0],prepend=0))
@@ -71,6 +81,123 @@ def clear_teleports(a,max_speed = 500):
     a[dx > max_speed] = [np.nan,np.nan]
     a[dy > max_speed] = [np.nan,np.nan]
     return a
+
+## Takes Frames x XY
+## This ideally occurs after getting best score, before interp
+def clear_teleports_(a,track_occupancy=None,instance_scores=None,max_distance = 300,min_track=2):
+    a = np.array(a)
+    n_frames = len(a)
+    if track_occupancy is None:
+        track_occupancy = (1- np.isnan(a[:,0]).T).astype(bool)
+    elif len(np.shape(track_occupancy)) > 1:
+        track_occupancy = np.max(track_occupancy,axis=0)
+    else:
+        track_occupancy = np.array(track_occupancy)
+    if len(np.shape(a)) > 2:
+        a = overlay_tracks(a,track_occupancy,instance_scores)
+        track_occupancy = (1- np.isnan(a[:,0]).T).astype(bool)
+
+    #gaps = np.argwhere(track_occupancy == False)[:,0]
+    
+    start_ = np.argmax(track_occupancy)  ## get's start of first detection
+    stop_ = np.argmax(track_occupancy[start_:] == False) + start_ ## Stop of first detection
+    next_ = np.argmax(track_occupancy[stop_:] == True) + stop_ ## Get next detection (start of 2nd)
+    last_ = stop_ - 1
+    
+    while start_ < n_frames:
+## Lots of opportunities for indexing errors here...
+        start_ = next_
+        stop_ = np.argmax(track_occupancy[start_:] == False) + start_
+        stop_check = np.max(track_occupancy[start_:] == False)
+        next_ = np.argmax(track_occupancy[stop_:] == True) + stop_
+        if not stop_check:
+            break
+        if stop_ - start_ < min_track:
+            a[start_:stop_] = np.nan
+        last_points = a[last_]
+        start_points = a[start_]
+        stop_points = a[stop_ - 1]
+        next_points = a[next_]
+        last_shift = np.linalg.norm(start_points - last_points)
+        next_shift = np.linalg.norm(next_points - stop_points)
+
+        #print(start_,':')
+        #print(start_points,last_points,last_shift)
+        #print(last_shift,next_shift)
+        #print('\n')
+        if last_shift < max_distance or next_shift < max_distance: ## Could be more clever here...
+## If at least one shift is reasonble, keep going
+            #print('this one looks good')
+            last_ = stop_-1
+            start_ = next_
+        else: 
+## Delete this section
+            #print('killing this one')
+            #print(start_,stop_)
+## Move the pointers
+            track_occupancy[start_:stop_] = False
+            a[start_:stop_] = np.nan
+            next_ = np.argmax(track_occupancy[last_:] == True) + last_
+        #import pdb;pdb.set_trace()
+# You probably still have the tracks...
+    return a,track_occupancy 
+
+## Takes Frames x XY x Tracks
+## Outputs Frames x XY to give you one best track
+def overlay_tracks(a,track_occupancy=None,instance_scores = None,min_track = 2):
+    if track_occupancy is None:
+        track_occupancy = (1- np.isnan(a[:,0]).T).astype(bool)
+    else:
+        track_occupancy = np.array(track_occupancy)
+
+    if len(np.shape(a)) == 4:
+        a = np.nanmean(a,axis=1) ## compress along nodes if needed
+    else:
+        a = np.array(a)
+
+    n_tracks = a.shape[2]
+    n_frames = a.shape[0]
+    single_track = np.empty([n_frames,2])
+    single_track.fill(np.nan)
+    sum_occupancy = np.sum(track_occupancy,0)
+    max_occupancy = np.max(track_occupancy,0)
+    for t in range(n_tracks):
+        frames = track_occupancy[t] == True
+        if len(frames) == 0:
+            continue ## this happens when the track is deleted
+        if len(frames) < min_track:
+            track_occupancy[frames] = False
+        track = a[frames,:,t] 
+        overlapping_frames = sum_occupancy[frames] > 1
+        #import pdb; pdb.set_trace()
+        if True in overlapping_frames and instance_scores is not None:
+            ## We can pick the best frames: 
+            frames_to_check = np.argwhere((sum_occupancy > 1) & (frames == 1))[:,0]
+            for f in frames_to_check:
+                t_score = instance_scores[f,t]
+                competitors = np.argwhere(track_occupancy[:,f])[:,0]
+                for c_ in competitors:
+                    if c_ == t:
+                        continue
+                    #c_score = np.nanmean(instance_scores[:,c_]) ## mean of entire track
+                    c_score = instance_scores[f,c_] ## score at point
+                    if t_score > c_score:
+                        track_occupancy[c_,f] = False
+                    elif t_score < c_score:
+                        track_occupancy[t,f] = False
+    for t in range(n_tracks):
+        frames = track_occupancy[t] == 1
+        if len(frames) == 0:
+            continue
+        else:
+            track = a[frames,:,t]
+            single_track[frames] = track
+    return single_track,track_occupancy
+
+## Same thing, but for a tank divided in quadrants
+def overlay_tracks_quad(a,track_occupancy,instance_scores,center_point):
+    print('This does not work yet')
+    return 0
 
 def nan_helper(y):
     return np.isnan(y), lambda z: z.nonzero()[0]
