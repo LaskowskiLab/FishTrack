4b05b34f3f6eb0c23120118d4dd27f1c68d9d00b
aperkes <perkes.ammon@gmail.com>
2024-01-19 15:15:19 -0800

    starts from beginning using date parsing to save time

--- recording/src/box_checkin.sh
+++ recording/src/box_checkin.sh
@@ -9,6 +9,10 @@ if [[ $pi_name == "rypi" ]]; then
     pi_name='pi01'
 fi
 
+hour=$(date +"%H")
+dhour="$((hour-6))"
+dseconds="$((dhour*60*60))"
+
 date > /home/pi/recording/hourly_check.txt
 pgrep raspivid >> /home/pi/recording/hourly_check.txt
 echo "(a number means it's recording, if there's no number it's not)" >> /home/pi/recording/hourly_check.txt
@@ -18,8 +22,11 @@ grep 'token' /home/pi/recording/cronlog.log | head -1 | grep 'token' | cut -c -5
 grep 'tvservice' /home/pi/recording/cronlog.log | head -1 >> /home/pi/recording/hourly_check.txt
 
 ## Grab most recent video clip? 
-ffmpeg -framerate 1 -sseof -2 -i /home/pi/recording/current.link -update 1 -q:v 1 /home/pi/recording/recent_cap.jpg -y
+#ffmpeg -framerate 1 -sseof -2 -i /home/pi/recording/current.link -update 1 -q:v 1 /home/pi/recording/recent_cap.jpg -y
+ffmpeg -framerate 1 -i ~/recording/current.link -ss $dseconds -vf "scale=iw*sar:ih,setsar=1" -vframes 1 test.png -y
+
 rclone copy /home/pi/recording/recent_cap.jpg AmazonBox:/pivideos/$pi_name/_monitoring_
+
 
 ## Copies and deletes the *.zip files in the recording directory (without checking sub directories)
 rclone copy /home/pi/recording/hourly_check.txt AmazonBox:/pivideos/$pi_name/_monitoring_
