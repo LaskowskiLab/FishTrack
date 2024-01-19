744968f215ddd99229715c6a7dfc81b7f1f574cf
aperkes <perkes.ammon@gmail.com>
2024-01-19 14:35:32 -0800

    new monitoring folder so its not so hard to find

--- recording/src/box_checkin.sh
+++ recording/src/box_checkin.sh
@@ -19,7 +19,7 @@ grep 'tvservice' /home/pi/recording/cronlog.log | head -1 >> /home/pi/recording/
 
 ## Grab most recent video clip? 
 ffmpeg -framerate 1 -sseof -2 -i /home/pi/recording/current.link -update 1 -q:v 1 /home/pi/recording/recent_cap.jpg
-rclone copy /home/pi/recording/recent_cap.jpg AmazonBox:/pivideos/$pi_name
+rclone copy /home/pi/recording/recent_cap.jpg AmazonBox:/pivideos/$pi_name/_monitoring_
 
 ## Copies and deletes the *.zip files in the recording directory (without checking sub directories)
-rclone copy /home/pi/recording/hourly_check.txt AmazonBox:/pivideos/$pi_name
+rclone copy /home/pi/recording/hourly_check.txt AmazonBox:/pivideos/$pi_name/_monitoring_
