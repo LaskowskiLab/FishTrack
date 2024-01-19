5741bf2705ba72a7dc4b6a162336e900a127c45f
aperkes <perkes.ammon@gmail.com>
2024-01-19 14:25:42 -0800

    added live checking, a commented-out line for rpicam-vid

--- recording/src/box_checkin.sh
+++ recording/src/box_checkin.sh
@@ -17,5 +17,9 @@ grep 'mmal' /home/pi/recording/cronlog.log | head -1 >> /home/pi/recording/hourl
 grep 'token' /home/pi/recording/cronlog.log | head -1 | grep 'token' | cut -c -52 >> /home/pi/recording/hourly_check.txt
 grep 'tvservice' /home/pi/recording/cronlog.log | head -1 >> /home/pi/recording/hourly_check.txt
 
+## Grab most recent video clip? 
+ffmpeg -framerate 1 -sseof -2 -i /home/pi/recording/current.link -update 1 -q:v 1 /home/pi/recording/recent_cap.jpg
+rclone copy /home/pi/recording/recent_cap.jpg AmazonBox:/pivideos/$pi_name
+
 ## Copies and deletes the *.zip files in the recording directory (without checking sub directories)
 rclone copy /home/pi/recording/hourly_check.txt AmazonBox:/pivideos/$pi_name
