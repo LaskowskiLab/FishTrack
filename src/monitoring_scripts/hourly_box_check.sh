7d66384bc05f951ca36fd3f1c7d68e8a54cae2c2
aperkes <perkes.ammon@gmail.com>
2024-02-15 09:08:36 -0800

    tidied up some unnecessary output

--- src/monitoring_scripts/hourly_box_check.sh
+++ src/monitoring_scripts/hourly_box_check.sh
@@ -13,14 +13,15 @@ for d in $dir_list; do
     if [[ "epi" == *"$d"* ]]; then
         continue
     fi
-    echo /pivideos/"$d"_monitoring_/hourly_check.txt
-    rclone cat aperkes:/pivideos/"$d"hourly_check.txt
-    rclone cat aperkes:/pivideos/"$d"hourly_check.txt > $tmp_path
+    #echo /pivideos/"$d"_monitoring_/hourly_check.txt
+    #rclone cat aperkes:/pivideos/"$d"hourly_check.txt
+    rclone cat aperkes:/pivideos/"$d"_monitoring_/hourly_check.txt > $tmp_path
+    #cat $tmp_path
 
     tmp_cat="$(cat $tmp_path)"
     tmp_l=$(wc -l < $tmp_path)
-    echo $tmp_l
     echo $tmp_cat
+    echo $tmp_l
     if [ $tmp_l -gt "3" ]; then
         bash /home/ammon/Documents/Scripts/FishTrack/src/send_mail.sh "Sorry to interrupt, but you should check on $d ASAP, it looks like something is wrong:" 
         while IFS= read -r line; do
