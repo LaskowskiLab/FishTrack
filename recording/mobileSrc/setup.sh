9d23bf7f2354c7945811c1d969332c81302a7c31
aperkes <perkes.ammon@gmail.com>
2024-04-09 17:29:13 -0700

    little setup script to get everything ready

---
+++ recording/mobileSrc/setup.sh
@@ -0,0 +1,5 @@
+cp ~/recording/mobileSrc ~/.bash_aliases
+cp ~/recording/mobielSrc/readme.txt ~/recording
+echo "" >> ~/.bashrc
+echo "*** Welcome to $HOSTNAME ***" >> ~/.bashrc
+echo "type help_me for shortcuts" >> ~/.bashrc
