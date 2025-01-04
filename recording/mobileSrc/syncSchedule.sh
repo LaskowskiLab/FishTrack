0f110bac3a93e73dc7d68a47dee6d48bfa1ec9ac
aperkes <perkes.ammon@gmail.com>
2025-01-03 16:54:57 -0800

    updated monitoring scripts, getting ready for beta testing

--- recording/mobileSrc/syncSchedule.sh
+++ recording/mobileSrc/syncSchedule.sh
@@ -1,10 +1,10 @@
 
-recordingDir="/home/ammon/Documents/Scripts/FishTrack/recording/"
+recordingDir="/home/pi/recording/"
 srcDir=$recordingDir"mobileSrc/"
-echo rclone copy aperkes:pivideos/ScheduleTest.csv $srcDir
+rclone copy AmazonBox:piManager/ScheduleTest.csv $srcDir
 pi_name=${HOSTNAME: -4}
-piLine="$(cat $srcDir"ScheduleTest.csv" | grep "pi24")"
-#piLine="$(cat $srcDir"ScheduleTest.csv" | grep $pi_name)"
+#piLine="$(cat $srcDir"ScheduleTest.csv" | grep "pi24")"
+piLine="$(cat $srcDir"ScheduleTest.csv" | grep $pi_name)"
 echo $piLine
 ## split and strip pi line to get pi,suffix,config
 
@@ -15,22 +15,20 @@ config=${suffix#*,}
 suffix=${suffix%%,*}
 
 config=${config%%,*}
-echo $pi
-echo $suffix
-echo $config
 
 if [ "${config,,}" = "on" ]; then
     echo "On!"
-    echo bash $srcDir"schedule_config.sh" $srcDir"configs/default.config"
+    bash $srcDir"schedule_on.sh"
 elif [ "${config,,}" = "off" ]; then
     echo "Off!" 
-    echo bash $srcDir"schedule_off.sh"
+    bash $srcDir"schedule_off.sh"
 elif [ -f $srcDir"configs/"$config ]; then 
     echo Found it! 
-    echo bash $srcDir"schedule_config.sh" $srcDir"configs/"$config
+    crontab $srcDir"base_cron.txt"
+    bash $srcDir"schedule_config.sh" $srcDir"configs/"$config
 else
-    echo Count not fine config
-    echo bash $srcDir"send_mail.sh" "***Warning*** Could not find config for $pi"
+    echo Count not find config
+    bash $srcDir"send_mail.sh" "***Warning*** Could not find config for $pi"
 fi
 
-echo bash $srcDir"set_suffix.sh" $suffix
+bash $srcDir"set_suffix.sh" $suffix
