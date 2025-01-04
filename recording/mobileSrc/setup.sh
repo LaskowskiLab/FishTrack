5bb2d895902a68f082d6bfffb01b201c1bd68548
aperkes <perkes.ammon@gmail.com>
2025-01-03 17:11:46 -0800

    fixed another typo

--- recording/mobileSrc/setup.sh
+++ recording/mobileSrc/setup.sh
@@ -1,5 +1,5 @@
 cp ~/recording/mobileSrc/mobile_aliases.txt ~/.bash_aliases
 cp ~/recording/mobileSrc/readme.txt ~/recording
 echo "" >> ~/.bashrc
-echo "echo *** Welcome to $HOSTNAME ***" >> ~/.bashrc
+echo "echo '*** Welcome to $HOSTNAME ***'" >> ~/.bashrc
 echo "echo type help_me for shortcuts" >> ~/.bashrc
