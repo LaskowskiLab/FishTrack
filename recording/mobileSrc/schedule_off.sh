2a5f4c22029e94d659690d7b2040e916c859ff85
aperkes <perkes.ammon@gmail.com>
2025-01-13 14:54:41 -0800

    updated scheduling to get the newer crontabs

--- recording/mobileSrc/schedule_off.sh
+++ recording/mobileSrc/schedule_off.sh
@@ -4,8 +4,8 @@
 suffix=${1:-PAUSED}
 
 ## This shouldn't matter, but is a backup against people forgetting
-bash ~/recording/src/set_suffix.sh $suffix
+bash ~/recording/mobileSrc/set_suffix.sh $suffix
 
 ## Set crontab to the schedule that doesn't record 
-crontab ~/recording/src/crontabs/crontab-pause.txt
+crontab ~/recording/mobileSrc/crontabs/pause-cron.txt
 
