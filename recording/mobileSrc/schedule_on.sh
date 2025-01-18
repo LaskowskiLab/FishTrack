c4466f40cb08f8b28cce4efa5578449782da0c01
aperkes <perkes.ammon@gmail.com>
2025-01-18 09:06:52 -0800

    changed hour handling to work

--- recording/mobileSrc/schedule_on.sh
+++ recording/mobileSrc/schedule_on.sh
@@ -10,9 +10,9 @@ bash /home/pi/recording/mobileSrc/set_suffix.sh $suffix
 
 ## Set crontab to the schedule that runs the pi
 ## To prevent annoying messages, this only turns it on in the evening
-current_time=$(date +%H:%M)
-if [[ "$current_time" > "18:15" ]]; then
+current_time=$(date +%H)
+if [[ "$current_time" > "18" ]]; then
     crontab /home/pi/recording/mobileSrc/crontabs/base_cron.txt
 else
-    crontab /home/pi/recording/mobileSrc/crontabs/base_cron.txt | at 18:15
+    crontab /home/pi/recording/mobileSrc/crontabs/base_cron.txt | at 19
 fi
