29f52787cf246a60f2bfad58d709b66a76e991a5
aperkes <perkes.ammon@gmail.com>
2025-01-22 14:07:52 -0800

    more flexible handling of other checks

--- src/monitoring_scripts/check_checks.sh
+++ src/monitoring_scripts/check_checks.sh
@@ -7,9 +7,20 @@ match_string="(a number means it's recording, if there's no number it's not)"
 schedule_file="/tmp/ScheduleTest.csv"
 
 for file in "$check_dir"/*; do
-    pi_n=${file##*pi}
-    pi_n=${pi_n%%.*}
-    pi="pi"$pi_n
+## But would be easy enough to vix
+    if [[ $file != *"pi"* ]]; then
+        v_n=${file##*V}
+        v_n=${v_n%%.*}
+        pi='V'$v_n
+        #echo "Skipping $file, $pi"
+        #continue
+    else
+        pi_n=${file##*pi}
+        pi_n=${pi_n%%.*}
+        pi="pi"$pi_n
+
+    fi
+
     echo $pi
     day_hour=$( date +"%d%H" )
     if [[ -f "$file" ]]; then
@@ -32,9 +43,10 @@ for file in "$check_dir"/*; do
             #echo $pi_sched
             #off_check=$(grep ",off," $pi_sched)
             #echo $pi_sched $off_check
-            if [[ "$pi_sched" != *",off,"* ]]; then
+            if [[ "$pi_sched" == *",on,"* ]]; then
                 echo "Should be recording!"
                 bash ~/Documents/Scripts/FishTrack/src/send_mail.sh "*** ALERT!! *** $pi has stopped recording, but seems to be scheduled"
+###  Need to add in what to do for configs...
             else
                 echo "But that's fine"
             fi
