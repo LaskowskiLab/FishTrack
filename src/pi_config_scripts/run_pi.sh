cc83d66bb7da17d4595fda5b25fb5d08eda28bac
aperkes <perkes.ammon@gmail.com>
2025-05-14 10:20:52 -0700

    changed to current.config

--- src/pi_config_scripts/run_pi.sh
+++ src/pi_config_scripts/run_pi.sh
@@ -6,7 +6,7 @@ i=pi@$pi_family$pi.local
 
 echo $i
 ssh -o StrictHostKeyChecking=no $i << EOF
-    nohup bash ~/recording/mobileSrc/watch_mobile.sh > /home/pi/recording/cronlog.log 2>&1 </dev/null &
+    nohup bash ~/recording/mobileSrc/watch_mobile.sh ~/recording/mobileSrc/current.config > /home/pi/recording/cronlog.log 2>&1 </dev/null &
     exit
 EOF
 echo 'End ' $i
